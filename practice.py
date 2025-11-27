1c344c95a221b025356fdb3fec2852d6335d9867
jay <bdj2498@gmail.com>
2025-11-27 17:39:10 +0530

    paractice for pandas

--- practice.py
+++ practice.py
@@ -19,3 +19,27 @@ mydataset = {
 myvar = pd.DataFrame(mydataset)
 
 print(myvar)
+a = [1,73,2]
+se = pd.Series(a,index=["x", "y", "z"])
+print(se)
+# print(se[0])
+# print(se)
+c = ["dhanu","swami","simha"]
+da = pd.Series(c,index=["son","father","mother"])
+print(da)
+
+import pandas as pd
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories)
+
+print(myvar)
+
+import pandas as pd
+
+calories = {"da1": 420, "da2": 380, "day3": 390}
+
+myvar = pd.Series(calories, index = ["da", "da2"])
+
+print(myvar)
