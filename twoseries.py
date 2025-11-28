b4a9b8cfa6631a11e0c4f7417e0c986414f2fe0c
jay <bdj2498@gmail.com>
2025-11-28 11:32:11 +0530

    update the code

--- twoseries.py
+++ twoseries.py
@@ -1,9 +1,8 @@
-# 
+# # 
+# # import pandas as pd 
+# # df = pd.read_csv("data.csv")
+# # new_df = df.dropna()
+# # print(new_df.to_string())
 # import pandas as pd 
-# df = pd.read_csv("data.csv")
-# new_df = df.dropna()
-# print(new_df.to_string())
-import pandas as pd 
-df = pd.read_csv(data.csv)
-df.fillna({"calarioes":130},inplace =True)
-
+# df = pd.read_csv(data.csv)
+# df.fillna({"calarioes":130},inplace =True)
